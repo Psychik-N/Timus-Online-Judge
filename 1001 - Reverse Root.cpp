ab30755e826f26c279587d6e58e2bbf9118d65bf
Psychik-N <57167867+Psychik-N@users.noreply.github.com>
2020-04-25 17:20:23 +0530

    Create 1001 - Reverse Root.cpp

---
+++ 1001 - Reverse Root.cpp
@@ -0,0 +1,52 @@
+/*
+	Idea :
+	1) Envolves the basic concept of how to know where the input end, this can be done
+	-- checking for EOF. Also avoid using fastIO in this problem.
+*/
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define v vector
+#define ALL(x) x.begin(),x.end()
+#define rALL(x) x.rbegin(),x.rend()
+#define endl '\n'
+#define DEBUG
+
+template<typename T> void DBG(const char* name, T&& H) {
+	cerr << name << " = " << H << ')' << endl;
+}
+
+template<typename T, typename... Args> void DBG(const char* names, T&& H, Args&&... args) {
+	const char* NEXT = strchr(names+1, ',');
+	cerr.write(names, NEXT-names) << " = " << H << " |";
+	DBG(NEXT+1, args...);
+}
+
+#ifdef DEBUG
+	#define dbg(...) cerr << __FUNCTION__ << ' ' <<__LINE__ << " : (", DBG(#__VA_ARGS__, __VA_ARGS__)
+#else
+	#define dbg(...)
+#endif
+
+using ll = long long;
+using ld = long double;
+
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+int main() {
+	vector<ld> numbers;
+	ll N;
+	while(scanf("%lld", &N) != EOF) {
+		numbers.push_back(sqrt(ld(N)));
+	}
+	reverse(ALL(numbers));
+	cout << fixed << setprecision(4);
+	for(auto i : numbers) cout << i << endl;
+	
+	return 0;
+}
