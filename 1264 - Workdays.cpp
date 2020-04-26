6ccaf8a7c85260a965043b992434e3f3931c1a04
Psychik-N <57167867+Psychik-N@users.noreply.github.com>
2020-04-26 16:01:40 +0530

    Create 1264 - Workdays.cpp

---
+++ 1264 - Workdays.cpp
@@ -0,0 +1,50 @@
+/*
+	Idea :
+	1) N*(M+1), make note for the overflow.
+*/
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define v vector
+#define ALL(x) x.begin(),x.end()
+#define rALL(x) x.rbegin(),x.rend()
+#define endl '\n'
+#define DEBUG
+
+template<typename T> void DBG(const char* name, T&& H) {
+	cerr << name << " = " << H << ')' << endl;
+}
+
+template<typename T, typename... Args> void DBG(const char* names, T&& H, Args&&... args) {
+	const char* NEXT = strchr(names+1, ',');
+	cerr.write(names, NEXT-names) << " = " << H << " |";
+	DBG(NEXT+1, args...);
+}
+
+#ifdef DEBUG
+	#define dbg(...) cerr << __FUNCTION__ << ' ' <<__LINE__ << " : (", DBG(#__VA_ARGS__, __VA_ARGS__)
+#else
+	#define dbg(...)
+#endif
+
+using ll = long long;
+using ld = long double;
+
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll N, M;
+	cin >> N >> M;
+	cout << (N * (M+1)) << endl;
+	
+	return 0;
+}
